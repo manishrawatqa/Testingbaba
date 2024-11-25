f8ad66f5eff07150ba2c386499e29a3f1469118d
manishrawatqa <manishrawatqa@gmail.com>
2024-11-25 16:29:35 +0530

    Add files via upload

---
+++ src/main/java/baselibrary/BaseLibrary.java
@@ -0,0 +1,17 @@
+package baselibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseLibrary {
+
+	@Test
+	public void launchurl()
+	{
+		//WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+	}
+}
